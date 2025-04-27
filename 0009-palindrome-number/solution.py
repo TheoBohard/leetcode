bd182d2820fbcaa19e0cffb742832ba5ce1b352e
Théo Bohard <theobohard00@gmail.com>
2025-04-28 00:04:26 +0200

    Rename folder to have good order on Github

--- 0009-palindrome-number/solution.py
+++ 0009-palindrome-number/solution.py
@@ -6,6 +6,6 @@ class Solution:
         reversed_half = 0
         while x > reversed_half:
             reversed_half = reversed_half * 10 + x % 10
-            x //= 10  # Remove the last digit from x
+            x //= 10
 
         return x == reversed_half or x == reversed_half // 10
